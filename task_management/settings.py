ca592b76877f639e7556058da3341eba15059b61
Maxim Gordilov <m.gordilov@yahoo.com>
2024-02-10 20:41:16 +0000

    Set up staticfiles hosting

--- task_management/settings.py
+++ task_management/settings.py
@@ -61,6 +61,7 @@ EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS'] #App Password set from gmail, Sto
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
--- task_management/urls.py
+++ task_management/urls.py
@@ -32,5 +32,7 @@ urlpatterns = [
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
