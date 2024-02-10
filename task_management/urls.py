e42de59367ab5a5c7f5506ee139588db8ef1e10d
Maxim Gordilov <m.gordilov@yahoo.com>
2024-02-10 21:45:21 +0000

    Modifying profile page

--- task_management/urls.py
+++ task_management/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('tm_app.urls')),
     path('', include('users.urls')),
 
